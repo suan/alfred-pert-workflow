f8629c2332bce5f1de42b696f8ae0045c2d6a6eb
Suan-Aik Yeo <yeosuanaik@gmail.com>
2014-02-14 00:14:09 -0600

    add std devs and nicer rounding

--- README.md
+++ README.md
@@ -15,6 +15,15 @@ Example:
 
 If \<Enter\> is pressed the output is copied to your clipboard.
 
+## Configuration
+By default, this calculator adds 2 standard deviations to the regular `(O + 4M + P) / 6` PERT formula to achieve a 97.5% estimate accuracy.
+
+You can change how many standard deviations are added by viewing the workflow in Alfred, clicking the first "Script Filter" box in the diagram, and modifying the `STD_DEVS` variable value in the "Script:" box.
+
+The calculated estimate is also rounded to the nearest 0.25.
+
+_(You can learn more about the effect adding different standard deviations has on prediction accuracy [here](http://eyelightmedia.blogspot.com/2009/06/value-of-pert-estimating.html))_
+
 ## Credits
 [Alfred 2 Workflow Ruby Template](https://github.com/zhaocai/alfred2-ruby-template)
 
--- workflow/main.rb
+++ workflow/main.rb
@@ -6,14 +6,27 @@ require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
 require "bundle/bundler/setup"
 require "alfred"
 
-def calc_pert(optimistic, most_likely, pessimistic)
-  (optimistic.to_f + 4*most_likely.to_f + pessimistic.to_f) / 6.0
+def calc_pert(std_devs, optimistic, most_likely, pessimistic)
+  weighted_mean(optimistic.to_f, most_likely.to_f, pessimistic.to_f) +
+    std_devs.to_f * std_dev(optimistic.to_f, pessimistic.to_f)
+end
+
+def weighted_mean(optimistic, most_likely, pessimistic)
+  (optimistic + 4*most_likely + pessimistic) / 6.0
+end
+
+def std_dev(optimistic, pessimistic)
+  (pessimistic - optimistic) / 6.0
+end
+
+def round_to_nearest_4th(num)
+  (num * 4.0).round / 4.0
 end
 
 Alfred.with_friendly_error do |alfred|
   fb = alfred.feedback
-  if ARGV.size == 3
-    fb.add_item(:title => sprintf('%.2f', calc_pert(*ARGV)))
+  if ARGV.size == 4
+    fb.add_item(:title => round_to_nearest_4th(calc_pert *ARGV).to_s)
   else
     fb.add_item(:title    => "Usage: pert <optimistic> <most likely> <pessimistic>",
                 :subtitle => "Example: pert 2 3 5")
